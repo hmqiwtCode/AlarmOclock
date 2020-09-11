609b9c10b8eaef7208ef891853326d162a74b738
Quy <huynhminhquy1999@gmail.com>
2020-09-11 18:00:10 +0700

    Error

--- app/src/main/java/com/quy/alarmoclock/RingtonePlayingService.java
+++ app/src/main/java/com/quy/alarmoclock/RingtonePlayingService.java
@@ -8,6 +8,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
@@ -15,12 +16,19 @@ import androidx.annotation.Nullable;
 public class RingtonePlayingService extends Service {
     private static final String NOTIFICATION_CHANNEL_ID = "10101";
     private static final CharSequence NOTIFICATION_CHANNEL_NAME = "APPOCLOCK" ;
+
     MediaPlayer mediaPlayer;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        startForeground(11,new Notification());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            configNoti();
+            Notification notification = new Notification.Builder(this).setContentTitle("ForeGround").setContentText("Work").setSmallIcon(R.drawable.ic_call).setChannelId("10101").build();
+            startForeground(2, notification);
+        }
+        else
+            startForeground(1, new Notification());
     }
 
     @Nullable
@@ -35,17 +43,9 @@ public class RingtonePlayingService extends Service {
         String status = intent.getExtras().getString("status");
         Intent intent1 = new Intent(this.getApplicationContext(), MainActivity.class);
         PendingIntent pIntent = PendingIntent.getActivity(this, 1, intent1, 0);
-
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
+        NotificationManager manager  = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_LOW;
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance);
-            notificationChannel.enableLights(true);
-            notificationChannel.setLightColor(Color.RED);
-            notificationChannel.enableVibration(true);
-            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-            manager.createNotificationChannel(notificationChannel);
+            configNoti();
         }
         Notification notification = new Notification.Builder(this).setContentTitle("Hello").setContentText("Sao cung dc").setSmallIcon(R.drawable.ic_call).setChannelId("10101").build();
 
@@ -58,5 +58,16 @@ public class RingtonePlayingService extends Service {
             mediaPlayer.reset();
         }
         return START_STICKY;
+    }
+
+    private void configNoti(){
+        NotificationManager manager  = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance);
+        notificationChannel.enableLights(true);
+        notificationChannel.setLightColor(Color.RED);
+        notificationChannel.enableVibration(true);
+        notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        manager.createNotificationChannel(notificationChannel);
     }
 }
